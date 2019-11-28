bb44b2837b470072c8224031a2ae082059a5739d
3bodyZZ <abdullahgamer212@gmail.com>
2019-11-28 13:24:30 +0300

    added custom admin to the User model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import UserProfile, FriendNotification, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'is_active', 'is_staff')
+    list_filter = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
 admin.site.register(UserProfile)
 admin.site.register(FriendNotification)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
